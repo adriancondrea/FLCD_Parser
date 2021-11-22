f351ccc33786e74f08d5aa731e96988946f60ea7
Condrea Adrian <adriancondrea2000@gmail.com>
2021-11-22 19:35:38 +0200

    Add g1.txt - context-free grammar from course 6

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     print(f'valid cfg: {Grammar.checkValidCFG(g.N, g.E, g.P, g.S)}')
     print(f'set of non-terminals is: {g.N}')
     print(f'set of terminals is: {g.E}')
-    print('set of product rules is: ')
-    for key in g.P.keys():
-        for element in g.P[key]:
-            print(f'{key} -> {element[0]}')
+    print('set of production rules is: ')
+    for lhs in g.P.keys():
+        for rhs in g.P[lhs]:
+            print(f'{lhs} -> {rhs}')
     print(f'starting symbol is: {g.S}')
     print(g.P)
