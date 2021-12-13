2d00d79610c4b9c264d46c10117e691fb5f23fc4
Condrea Adrian <adriancondrea2000@gmail.com>
2021-12-13 16:34:52 +0200

    Lab 7 - implement parsing algorithm

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from grammar import Grammar
 from parser import Parser
 
-if __name__ == '__main__':
+
+def test_grammar():
     g = Grammar.readGrammarFromFile('g1.txt')
-    # print(f'valid cfg: {Grammar.checkValidCFG(g.N, g.E, g.P, g.S)}')
-    # print(f'set of non-terminals is: {g.N}')
-    # print(f'set of terminals is: {g.E}')
-    # print('set of production rules is: ')
-    # for lhs in g.P.keys():
-    #     for rhs in g.P[lhs]:
-    #         print(f'{lhs} -> {rhs}')
-    # print(f'starting symbol is: {g.S}')
-    # print(g.P)
+    print(f'valid cfg: {Grammar.checkValidCFG(g.N, g.E, g.P, g.S)}')
+    print(f'set of non-terminals is: {g.N}')
+    print(f'set of terminals is: {g.E}')
+    print('set of production rules is: ')
+    for lhs in g.P.keys():
+        for rhs in g.P[lhs]:
+            print(f'{lhs} -> {rhs}')
+    print(f'starting symbol is: {g.S}')
+    print(g.P)
+
+
+def test_parser_methods():
+    g = Grammar.readGrammarFromFile('g1.txt')
 
     print("test expand: ")
     parser = Parser(g, ['a', 'b', 'a', 'c'])
@@ -60,3 +65,15 @@ if __name__ == '__main__':
     print(parser)
     parser.another_try()
     print(parser, '\n')
+
+
+def test_parse():
+    g = Grammar.readGrammarFromFile('g1.txt')
+    parser = Parser(g, ['a', 'c', 'b', 'c'])
+    print(parser)
+    parser.parse()
+
+
+if __name__ == '__main__':
+    # test_parser_methods()
+    test_parse()
--- parser.py
+++ parser.py
@@ -8,15 +8,40 @@ class Parser:
         self.input_string = input_string
         self.input_string_initial_length = len(input_string)
 
+    def parse(self):
+        while self.s != 'f' and self.s != 'e':
+            if self.s == 'q':
+                if self.i == self.input_string_initial_length + 1 and not self.input_stack:
+                    self.success()
+                else:
+                    if self.input_stack[0] in self.grammar.N:
+                        self.expand()
+                    elif self.input_stack[0] == self.input_string[self.i - 1]:
+                        self.advance()
+                    else:
+                        self.momentary_insuccess()
+            else:
+                if self.s == 'b':
+                    if self.working_stack[-1][0] in self.grammar.E:
+                        self.back()
+                    else:
+                        self.another_try()
+
+        if self.s == 'e':
+            print('error!')
+        else:
+            print('message accepted!')
+
     def expand(self):
         print("expand")
         head = self.input_stack[0]
         if head in self.grammar.N:
-            self.input_stack.pop()
+            self.input_stack.pop(0)
             self.working_stack.append((head, 1))
             # self.input_stack.insert(0, self.grammar.P[head][0])
             self.input_stack = self.grammar.P[head][0][0].split(' ') + self.input_stack
             # self.input_stack.insert(0, self.grammar.P[head][0][0].split(' '))
+            print(self)
         else:
             print('head of input stack is not a nonterminal!')
 
@@ -28,6 +53,7 @@ class Parser:
             self.input_stack.pop(0)
             self.working_stack.append((head, self.i))
             self.i += 1
+            print(self)
         else:
             print("Can not perform advance!")
 
@@ -36,6 +62,7 @@ class Parser:
         if not (head in self.grammar.E and head == self.input_string[self.i - 1]):
             print("momentary insuccess")
             self.s = "b"
+            print(self)
 
     def back(self):
         head = self.working_stack[-1][0]
@@ -44,6 +71,7 @@ class Parser:
             self.i = self.i - 1
             self.input_stack = [head] + self.input_stack
             self.working_stack.pop()
+            print(self)
 
     def another_try(self):
         head, production_index = self.working_stack[-1]
@@ -79,15 +107,19 @@ class Parser:
                 self.s = 'b'
                 nonterminal = self.working_stack.pop()
                 self.input_stack = [nonterminal[0]] + self.input_stack
+                print(self)
                 return
 
             self.working_stack[-1] = (head, production_index + 1)
             self.input_stack = next_production + self.input_stack
+            self.s = 'q'
+            print(self)
 
     def success(self):
         if self.s == 'q' and self.i == self.input_string_initial_length + 1 and len(self.input_stack) == 0:
             print('success')
             self.s = 'f'
+            print(self)
 
     def __str__(self):
         return f"({self.s}, {self.i}, {self.working_stack}, {self.input_stack})"
